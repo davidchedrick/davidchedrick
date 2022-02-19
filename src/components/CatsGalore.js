c43854d35f8bf8510ac22e59ff3b11d594adf78f
David <davidchedrick@gmail.com>
2022-02-18 20:48:05 -0600

    add details

--- src/components/CatsGalore.js
+++ src/components/CatsGalore.js
@@ -4,10 +4,14 @@ import ProjectCard from "./ProjectCards";
 function CatsGalore() {
 
     const img = catsGaloreImg
-    const text = "cat"
-    const list = "Made with: HTML, CSS, JavaScript"
+    const text = "Cats Galore is a JavaScript game. Built from scratch using vanilla JavaScript and HTML hosted on Netlify. "
     const link = "https://catsgalore.netlify.app/"
     const title = "Cats Galore"
+    const list =
+    <div>
+        <ul><li>HTML</li><li>CSS</li><li>JavaScript</li></ul><ul><li><a href="https://github.com/davidchedrick/phase_1_project">GitHub</a></li></ul>
+    </div>
+    
 
     return <ProjectCard img={img} title={title} text={text} list={list} link={link} /> 
 
--- src/components/CutieCats.js
+++ src/components/CutieCats.js
@@ -4,10 +4,13 @@ import ProjectCard from "./ProjectCards";
 function CutieCats() {
 
     const img = cutieCatImg
-    const text = "cat"
-    const list = "Made with: HTML, CSS, JavaScript"
+    const text = "Cutie Cats is a React single page application. Using client-side routing, with React-Router. The application has full CRUD capabilities. Front-end is hosted on Netlify and with the Back-end hosted on Heroku."
     const link = "https://cutiecats.netlify.app/"
     const title = "Cutie Cats"
+    const list =
+    <div> 
+        <ul><li>React</li><li>CSS</li><li>Bootstrap</li><li>Styled-Components</li><li>React-Router</li><li>uuid</li></ul><ul><li><a href="https://github.com/davidchedrick/phase2frontend/tree/main/phase-2-frontend">GitHub</a></li></ul>
+    </div>
     
     return <ProjectCard img={img} title={title} text={text} list={list} link={link} />
 
