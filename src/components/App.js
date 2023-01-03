f2993882d0fbd1684a82f630dfb89d2575ca5d6c
David Chedrick <davidchedrick@gmail.com>
2023-01-03 03:28:14 -0600

    added color toggle

--- src/components/App.js
+++ src/components/App.js
@@ -1,63 +1,18 @@
 import Header from "./Header";
 import ProjectArea from "./ProjectArea";
-import styled from "styled-components";
-
+import { useState } from "react";
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
 
-const GlobalStyles = styled.div`
-	/* @font-face {
-		font-family: "radnika_next";
-		src: url("/static/radnikanext-medium-webfont.woff2") format("woff2");
-		font-weight: normal;
-		font-style: normal;
-	} */
-	html {
-		--red: #ff0000;
-		--black: #393939;
-		--grey: #3a3a3a;
-		--gray: var(--grey);
-		--lightGrey: #e1e1e1;
-		--lightGray: var(--lightGrey);
-		--offWhite: #ededed;
-		--maxWidth: 1000px;
-		--bs: 0 12px 24px 0 rgba(0, 0, 0, 0.09);
-		box-sizing: border-box;
-		font-size: 10px;
-	}
-	*,
-	*:before,
-	*:after {
-		box-sizing: inherit;
-	}
-	body {
-		font-family: "radnika_next", --apple-system, BlinkMacSystemFont,
-			"Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans",
-			"Helvetica Neue", sans-serif;
-		padding: 0;
-		margin: 0;
-		font-size: 1.5rem;
-		line-height: 2;
-	}
-	a {
-		text-decoration: none;
-		color: var(--black);
-	}
-	a:hover {
-		text-decoration: underline;
-	}
-	button {
-		font-family: "radnika_next", --apple-system, BlinkMacSystemFont,
-			"Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans",
-			"Helvetica Neue", sans-serif;
-	}
-`;
-
 function App() {
+	const [isActive, setIsActive] = useState(false);
+	console.log("isActive: ", isActive);
+
 	return (
-		<GlobalStyles className="App">
-			<Header />
+		<div className={isActive ? "App" : "App dark-mode"}>
+			<Header setIsActive={setIsActive} />
 			<ProjectArea />
-		</GlobalStyles>
+		</div>
 	);
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,24 @@
 import styled from "styled-components";
 
-function Header() {
-	return <HeaderDiv>David Chedrick.</HeaderDiv>;
+function Header({ setIsActive }) {
+	function handleToggle() {
+		setIsActive(current => !current);
+	}
+
+	return (
+		<HeaderDiv>
+			<span>David Chedrick.</span>
+			<ToggleStyle>
+				<label className="switch">
+					<input
+						type="checkbox"
+						onClick={handleToggle}
+					/>
+					<span className="slider"></span>
+				</label>
+			</ToggleStyle>
+		</HeaderDiv>
+	);
 }
 
 const HeaderDiv = styled.div`
@@ -9,9 +26,68 @@ const HeaderDiv = styled.div`
 		"Lucida Sans", Arial, sans-serif;
 	text-align: center;
 	text-decoration: underline;
+	display: flex;
+	flex-direction: row;
 	padding: 20px;
+	justify-content: space-between;
 	margin: 20px;
 	font-size: 50px;
+`;
+
+const ToggleStyle = styled.div`
+	.switch {
+		font-size: 17px;
+		position: relative;
+		display: inline-block;
+		width: 3.5em;
+		height: 2em;
+	}
+
+	.slider {
+		position: absolute;
+		cursor: pointer;
+		top: 0;
+		left: 0;
+		right: 0;
+		bottom: 0;
+		background-color: rgb(0, 0, 0);
+		transition: 0.4s;
+		border-radius: 30px;
+	}
+
+	.slider:before {
+		position: absolute;
+		content: "";
+		height: 1.4em;
+		width: 1.4em;
+		border-radius: 20px;
+		left: 0.3em;
+		bottom: 0.3em;
+		background-color: rgb(250, 062, 250);
+		transition: 0.4s;
+	}
+
+	input:checked + .slider:before {
+		transform: translateX(1.5em);
+	}
+
+	.slider::after {
+		width: 20px;
+		height: 20px;
+		left: 20%;
+		top: 10%;
+		border-radius: 50%;
+		position: absolute;
+		content: "";
+		background-color: rgb(0, 0, 0);
+		transition: ease 0.4s;
+	}
+
+	input:checked + .slider::after {
+		left: 80%;
+		width: 0px;
+		height: 0px;
+	}
 `;
 
 export default Header;
