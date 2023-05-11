543f5444e3748549a49d183dd88c8a018f98483a
David Chedrick <davidchedrick@gmail.com>
2023-05-10 22:59:36 -0500

    cleaned up nav.js and header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import styled from "styled-components";
 import Nav from "./Nav";
 import Social from "./social/Social";
@@ -9,12 +9,9 @@ function Header() {
 		<>
 			<Social />
 			<HeaderDiv>
-				<Link
-					style={{ textDecoration: "none" }}
-					to="/"
-				>
+				<StyledLink to="/">
 					<span className="title">David Chedrick.</span>
-				</Link>
+				</StyledLink>
 				<Toggle />
 			</HeaderDiv>
 			<Nav />
@@ -22,25 +19,27 @@ function Header() {
 	);
 }
 
+const MOBILE_BREAKPOINT = 700;
+
 const HeaderDiv = styled.div`
 	font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
 		"Lucida Sans", Arial, sans-serif;
 	text-align: center;
-	/* text-decoration: none; */
 	display: flex;
 	flex-direction: row;
 	padding: 0px;
-	margin: 0px;
-	justify-content: space-between;
 	margin: 20px;
+	justify-content: space-between;
 	font-size: 50px;
 	color: #da1598;
-	@media (max-width: 700px) {
-		display: flex;
+	@media (max-width: ${MOBILE_BREAKPOINT}px) {
 		flex-direction: column-reverse;
 		font-size: 3em;
-		padding: 0px;
 	}
+`;
+
+const StyledLink = styled.RouterLink`
+	text-decoration: none;
 `;
 
 export default Header;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,13 @@
-import { Link } from "react-router-dom";
+import { Link as RouterLink } from "react-router-dom";
 import styled from "styled-components";
 
 export default function Nav() {
 	return (
 		<NavStyles>
-			<Link to="about">About</Link>
-			<Link to="/">Projects</Link>
-			<Link to="games">Games</Link>
-			<Link to="contact">Contact</Link>
+			<StyledLink to="about">About</StyledLink>
+			<StyledLink to="/">Projects</StyledLink>
+			<StyledLink to="games">Games</StyledLink>
+			<StyledLink to="contact">Contact</StyledLink>
 			<button
 				className="invisable-button"
 				disabled
@@ -18,71 +18,75 @@ export default function Nav() {
 	);
 }
 
+const MOBILE_BREAKPOINT = 700;
+const LARGE_SCREEN_BREAKPOINT = 2900;
+
+const StyledLink = styled(RouterLink)`
+	padding: 1rem 3rem;
+	display: flex;
+	align-items: center;
+	position: relative;
+	text-transform: uppercase;
+	font-weight: 800;
+	font-size: 1.2em;
+	background: none;
+	border: 0;
+	text-decoration: none;
+	cursor: pointer;
+	color: var(--blue);
+	-webkit-text-stroke: 0.5px white;
+
+	@media (max-width: ${MOBILE_BREAKPOINT}px) {
+		font-size: 14px;
+		padding: 0 10px;
+	}
+
+	&:before {
+		content: "";
+		width: 2px;
+		background: var(--pink);
+		height: 100%;
+		left: 0;
+		position: absolute;
+		transform: skew(-20deg);
+		top: 0;
+		bottom: 0;
+	}
+
+	&:after {
+		height: 2px;
+		background: var(--pink);
+		content: "";
+		width: 0;
+		position: absolute;
+		transform: translateX(-50%);
+		transition: width 0.4s;
+		transition-timing-function: cubic-bezier(1, -0.65, 0, 2.31);
+		left: 50%;
+		margin-top: 2rem;
+	}
+
+	&:hover,
+	&:focus {
+		outline: none;
+		text-decoration: none;
+		&:after {
+			width: calc(100% - 60px);
+		}
+	}
+`;
+
 const NavStyles = styled.ul`
 	margin: 0;
 	padding: 0;
 	display: flex;
 	font-size: 2rem;
+	border-top: 1px solid var(--pink);
+	border-bottom: 1px solid var(--pink);
+	width: 100%;
+	justify-content: space-evenly;
 
-	a,
-	button {
-		padding: 1rem 3rem;
-		display: flex;
-		align-items: center;
-		position: relative;
-		text-transform: uppercase;
-		font-weight: 800;
-		font-size: 1.2em;
-		background: none;
-		border: 0;
-		text-decoration: none;
-		cursor: pointer;
-		color: var(--blue);
-		-webkit-text-stroke: 0.5px white;
-
-		@media (max-width: 700px) {
-			font-size: 14px;
-			padding: 0 10px;
-		}
-		&:before {
-			content: "";
-			width: 2px;
-			background: var(--pink);
-			height: 100%;
-			left: 0;
-			position: absolute;
-			transform: skew(-20deg);
-			top: 0;
-			bottom: 0;
-		}
-		&:after {
-			height: 2px;
-			background: var(--pink);
-			content: "";
-			width: 0;
-			position: absolute;
-			transform: translateX(-50%);
-			transition: width 0.4s;
-			transition-timing-function: cubic-bezier(1, -0.65, 0, 2.31);
-			left: 50%;
-			margin-top: 2rem;
-		}
-		&:hover,
-		&:focus {
-			outline: none;
-			text-decoration: none;
-			&:after {
-				width: calc(100% - 60px);
-			}
-			@media (max-width: 700px) {
-			}
-		}
-	}
-	@media (max-width: 2900px) {
-		border-top: 1px solid var(--pink);
-		border-bottom: 1px solid var(--pink);
-		width: 100%;
-		justify-content: space-evenly;
+	@media (max-width: ${LARGE_SCREEN_BREAKPOINT}px) {
 		font-size: 1.5rem;
 	}
 
